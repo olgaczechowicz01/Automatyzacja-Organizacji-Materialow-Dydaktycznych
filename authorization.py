a946a6abd4c27dc0eb1d2cf57e59d7b40bc9bd62
olgaczechowicz01 <olga.czechowicz01@gmail.com>
2024-06-09 15:08:53 +0200

    authorization.py

--- authorization.py
+++ authorization.py
@@ -6,9 +6,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/classroom.announcements",
-          "https://www.googleapis.com/auth/classroom.courses"	,
+          "https://www.googleapis.com/auth/classroom.courses",
           "https://www.googleapis.com/auth/drive"]
 
 
@@ -17,12 +16,10 @@ def main():
   Prints the names of the first 10 courses the user has access to.
   """
   creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
+
   if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
+  # If there are no (valid) credentials available, makes the request for the user log in.
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
@@ -38,7 +35,7 @@ def main():
   try:
     service = build("classroom", "v1", credentials=creds)
 
-    # Call the Classroom API
+    # Calls on the Classroom API by requestin specific service.
     results = service.courses().list(pageSize=10).execute()
     courses = results.get("courses", [])
 
